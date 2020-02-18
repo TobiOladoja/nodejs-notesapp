1f984601b858ee503e5018de004f69357cadbbc8
TobiOladoja <tbfullstack@gmail.com>
2020-02-18 12:54:43 +0000

    Proccess of Accessing Property in JSON File

--- practicejson.js
+++ practicejson.js
@@ -1,14 +1,17 @@
-const book = {
-  title: 'The Title',
-  author: 'The Author'
-};
+const fs = require('fs');
+// const book = {
+//   title: 'The Title',
+//   author: 'The Author'
+// };
 
-// Convert the above object below into JSON, which is essentially nothing more than a string.
-// JSON.stringify takes in the object and gives me a JSON string back. **CONVERT TO JSON**
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('firstJSON.json', bookJSON);
 
-const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
-
-// JSON.prase is opposite of stringify, it takes in the JSON string and gives back an object. **CONVERT TO OBJECT**
-const parsedData = JSON.parse(bookJSON);
-console.log(parsedData);
+// Read The File In
+const dataBuffer = fs.readFileSync('firstJSON.json');
+// Converted that data into a standard string in Javascript
+const dataBufferJSON = dataBuffer.toString();
+// Then parsed that data into an object
+const data = JSON.parse(dataBufferJSON);
+// Finally, I accessed an property from the object.
+console.log(data.title);
