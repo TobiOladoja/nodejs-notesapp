cb7e274f2f159646e0eb35d01ea91a70d3a4fdca
TobiOladoja <tbfullstack@gmail.com>
2020-02-18 13:27:05 +0000

    Storing Data With JSON

--- practicejson.js
+++ practicejson.js
@@ -8,10 +8,27 @@ const fs = require('fs');
 // fs.writeFileSync('firstJSON.json', bookJSON);
 
 // Read The File In
+// const dataBuffer = fs.readFileSync('firstJSON.json');
+// // Converted that data into a standard string in Javascript
+// const dataBufferJSON = dataBuffer.toString();
+// // Then parsed that data into an object
+// const data = JSON.parse(dataBufferJSON);
+// // Finally, I accessed an property from the object.
+// console.log(data.title);
+
+// NEW CHALLENGE: work with JSON and File System
+
+// 1. Load and parse the JSON data
 const dataBuffer = fs.readFileSync('firstJSON.json');
-// Converted that data into a standard string in Javascript
 const dataBufferJSON = dataBuffer.toString();
-// Then parsed that data into an object
-const data = JSON.parse(dataBufferJSON);
-// Finally, I accessed an property from the object.
-console.log(data.title);
+const user = JSON.parse(dataBufferJSON);
+
+// 2. Change the name and age property using my info
+user.name = 'Tobi';
+user.age = 21;
+
+// 3. Stringify the changed object and overwrite the original data
+const userJSON = JSON.stringify(user);
+fs.writeFileSync('firstJSON.json', userJSON);
+
+// 4. Test work by viewing the data in the JSON file.
